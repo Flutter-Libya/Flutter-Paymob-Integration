1beb787504e33b373f8dce1d002a536c0563a873
Flutter-Libya <131480121+Flutter-Libya@users.noreply.github.com>
2023-11-28 09:11:26 +0200

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,11 @@
+
+
+
+
+
 import 'package:flutter/material.dart';
 import 'package:flutter_paymob_integration/home_screen.dart';
-
-void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+void main() {runApp(const MyApp());}
+class MyApp extends StatelessWidget {const MyApp({super.key});
   @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: HomeScreen(),
-    );
-  }
-}
+  Widget build(BuildContext context) {return const MaterialApp(home: HomeScreen(),);}}
